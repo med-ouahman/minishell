691ac35b39f4e8a9b017aa3df87f938bebd14c6e
Mohamed Ouahmane <mouahman@c2r6p2.1337.ma>
2025-07-05 14:53:20 +0100

    committing changed files.

--- executor/print_error.c
+++ executor/print_error.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aid-bray <aid-bray@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mouahman <mouahman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/30 14:56:01 by mouahman          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/07/04 21:19:54 by mouahman         ###   ########.fr       */
-=======
-/*   Updated: 2025/07/03 17:27:01 by aid-bray         ###   ########.fr       */
->>>>>>> 191da425410b72e73f73348b77e5aea2e344120c
+/*   Created: 2025/07/05 14:36:20 by mouahman          #+#    #+#             */
+/*   Updated: 2025/07/05 14:44:20 by mouahman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +14,5 @@
 
 void	print_file_error(char *__file, char *__err_msg)
 {
-<<<<<<< HEAD
-	printf("minishell: %s: %s\n", __file, __err_msg);
-=======
-	ft_printf_fd(2, "minishell: %s: %s\n", file, __err_msg);
->>>>>>> 191da425410b72e73f73348b77e5aea2e344120c
+	ft_printf_fd(2, "minishell: %s: %s\n", __file, __err_msg);
 }
